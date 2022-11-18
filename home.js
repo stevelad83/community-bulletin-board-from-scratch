d4b57b7ccf3877956fb0bcb4e758bde18ebc9d84
Steven Bryant <stevelad_83@yahoo.com>
2022-11-17 20:01:24 -0500

    everything functioning except new posts

--- home.js
+++ home.js
@@ -1,9 +1,10 @@
-import { fetchPosts, logout } from './fetch-utils.js';
+import { createNewPost, fetchPosts, logout } from './fetch-utils.js';
 import { renderPost } from './render-utils.js';
 
 const postsContainer = document.getElementById('posts-container');
 const signUpInBtn = document.getElementById('sign-up-in-button');
 const logOutBtn = document.getElementById('log-out-button');
+const submitButton = document.getElementById('submit-btn');
 
 window.addEventListener('load', async (e) => {
     e.preventDefault();
@@ -14,6 +15,16 @@ window.addEventListener('load', async (e) => {
         postsContainer.append(postDiv);
     }
 });
+
+// submitButton.addEventListener('click', async (e) => {
+//     e.preventDefault();
+//     const posts = await fetchPosts();
+
+//     for (let post of posts) {
+//         const postDiv = createNewPost(post);
+//         postsContainer.append(postDiv);
+//     }
+// });
 
 signUpInBtn.addEventListener('click', () => {
     location.replace('auth/index.html');
